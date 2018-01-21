2d43b3691b9c5d42b152401744b859d84cb9d6c3
Svoykina N <nadya.svojkina@gmail.com>
2018-01-22 00:54:40 +0300

    + table for get users

--- modules/assets/db/postgres/0000-base.sql
+++ modules/assets/db/postgres/0000-base.sql
@@ -1,24 +1,42 @@
--- +migrate Down
+-- +migrate Up
 DROP INDEX IF EXISTS users_nickname_index;
 DROP INDEX IF EXISTS users_email_index;
-DROP INDEX IF EXISTS forums_user_index;
-DROP INDEX IF EXISTS forums_slug_index;
-DROP INDEX IF EXISTS threads_slug_index;
-DROP INDEX IF EXISTS threads_forum_index;
-DROP INDEX IF EXISTS threads_owner_index;
-DROP INDEX IF EXISTS posts_thread_index;
-DROP INDEX IF EXISTS posts_parent_index;
-DROP INDEX IF EXISTS posts_author_index;
-DROP INDEX IF EXISTS posts_parent_path_index;
-DROP INDEX IF EXISTS votes_user_thread_index;
-DROP TABLE votes;
-DROP TABLE posts;
-DROP TABLE threads;
-DROP TABLE forums;
-DROP TABLE users;
 
 -- +migrate Up
-CREATE EXTENSION LTREE;
+DROP INDEX IF EXISTS forums_low_slug_index;
+DROP INDEX IF EXISTS forums_slug_index;
+
+-- +migrate Up
+DROP INDEX IF EXISTS threads_slug_index;
+DROP INDEX IF EXISTS threads_id_index;
+DROP INDEX IF EXISTS threads_forum_created_index;
+DROP INDEX IF EXISTS threads_forum_index;
+DROP INDEX IF EXISTS threads_author_index;
+
+-- +migrate Up
+DROP INDEX IF EXISTS posts_thread_index;
+DROP INDEX IF EXISTS posts_id_index;
+DROP INDEX IF EXISTS posts_thread_id_index;
+DROP INDEX IF EXISTS posts_author_index;
+DROP INDEX IF EXISTS posts_forum_index;
+DROP INDEX IF EXISTS posts_path_index;
+
+-- +migrate Up
+DROP INDEX IF EXISTS votes_user_thread_index;
+DROP INDEX IF EXISTS votes_thread_index;
+DROP INDEX IF EXISTS forum_users_slug_nickname_index;
+
+-- +migrate Up
+DROP TRIGGER IF EXISTS threads_tgr ON threads;
+DROP TRIGGER IF EXISTS parent_path_tgr ON posts;
+
+-- +migrate Up
+DROP TABLE IF EXISTS forum_users;
+DROP TABLE IF EXISTS votes;
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS threads;
+DROP TABLE IF EXISTS forums;
+DROP TABLE IF EXISTS users;
 
 -- +migrate Up
 CREATE TABLE IF NOT EXISTS users (
@@ -28,7 +46,7 @@ CREATE TABLE IF NOT EXISTS users (
   fullname VARCHAR(64) NOT NULL,
   nickname TEXT UNIQUE NOT NULL
 );
-CREATE UNIQUE INDEX IF NOT EXISTS users_low_nickname_index
+CREATE UNIQUE INDEX IF NOT EXISTS users_nickname_index
   ON users (lower(nickname));
 CREATE UNIQUE INDEX IF NOT EXISTS users_email_index
   ON users (lower(email));
@@ -66,7 +84,7 @@ CREATE INDEX IF NOT EXISTS threads_forum_index
   ON threads (forum);
 CREATE INDEX IF NOT EXISTS threads_forum_created_index
   ON threads (forum, created);
-CREATE INDEX IF NOT EXISTS threads_owner_index
+CREATE INDEX IF NOT EXISTS threads_author_index
   ON threads (author);
 
 -- +migrate Up
@@ -79,7 +97,8 @@ CREATE TABLE IF NOT EXISTS posts (
   is_edited BOOLEAN NOT NULL DEFAULT FALSE,
   message   TEXT NOT NULL,
   parent    BIGINT DEFAULT 0,
-  path      LTREE
+  path      BIGINT [],
+  root_id   BIGINT
 );
 CREATE INDEX IF NOT EXISTS posts_thread_id_index
   ON posts (thread, id);
@@ -97,31 +116,40 @@ CREATE INDEX IF NOT EXISTS posts_path_index
 -- +migrate StatementBegin
 CREATE OR REPLACE FUNCTION update_parent_path() RETURNS TRIGGER AS
 $update_parent_path$
-  DECLARE p LTREE;
   BEGIN
     IF (NEW.parent = 0)
       THEN
-        NEW.path = new.id :: TEXT :: LTREE;
-    ELSEIF TG_OP = 'INSERT'
-      THEN
-        SELECT posts.path || NEW.id :: TEXT FROM posts WHERE id = NEW.parent INTO p;
-        IF p IS NULL
-          THEN
-            RAISE EXCEPTION 'Invalid parent_id %', NEW.parent;
-        END IF;
-        NEW.path = p;
+        NEW.path = ARRAY[NEW.id];
+        NEW.root_id = NEW.id;
+      ELSE
+        NEW.path = (SELECT posts.path || NEW.id FROM posts WHERE id = NEW.parent);
+        NEW.root_id = NEW.path[1];
     END IF;
+    INSERT INTO forum_users (slug, nickname) (SELECT NEW.forum, NEW.author) ON CONFLICT(slug, nickname) DO NOTHING;
     RETURN NEW;
   END;
 $update_parent_path$
 LANGUAGE plpgsql;
 -- +migrate StatementEnd
 
-CREATE TRIGGER parent_path_tgr
-BEFORE INSERT
-ON posts
-FOR EACH ROW
-EXECUTE PROCEDURE update_parent_path();
+-- +migrate Up
+CREATE TRIGGER parent_path_tgr BEFORE INSERT ON posts
+FOR EACH ROW EXECUTE PROCEDURE update_parent_path();
+
+-- +migrate StatementBegin
+CREATE OR REPLACE FUNCTION update_threads() RETURNS TRIGGER AS
+$update_threads$
+  BEGIN
+    INSERT INTO forum_users (slug, nickname) (SELECT NEW.forum, NEW.author) ON CONFLICT(slug, nickname) DO NOTHING;
+    RETURN NEW;
+  END;
+$update_threads$
+LANGUAGE plpgsql;
+-- +migrate StatementEnd
+
+-- +migrate Up
+CREATE TRIGGER threads_tgr AFTER INSERT ON threads
+FOR EACH ROW EXECUTE PROCEDURE update_threads();
 
 -- +migrate Up
 CREATE TABLE IF NOT EXISTS votes (
@@ -134,3 +162,12 @@ CREATE UNIQUE INDEX votes_user_thread_index
   ON votes (lower(author), thread);
 CREATE INDEX votes_thread_index
   ON votes (thread);
+
+-- +migrate Up
+CREATE TABLE IF NOT EXISTS forum_users (
+  slug      TEXT NOT NULL,
+  nickname  TEXT NOT NULL,
+  UNIQUE(slug, nickname)
+);
+CREATE INDEX forum_users_slug_nickname_index
+  ON forum_users (slug);
